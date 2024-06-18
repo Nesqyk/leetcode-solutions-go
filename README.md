fe339a472eeab01caa384a02436aecd1112027fc
kskaneki1 <kskaneki5@gmail.com>
2024-06-18 18:48:13 +0800

    Two Sum; problem and solution.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # leetcode-solutions-go
 My personalized compilation of LeetCode problems that I have solved (mostly in Go)
+
+
---
+++ problem/001_twoSum.go
@@ -0,0 +1,13 @@
+package problem
+
+func TwoSum(nums []int, target int) []int {
+	numMap := make(map[int]int) // Create an empty map to store numbers and their indices.
+	for i, num := range nums {  // Loop over each number in the nums array with its index.
+		complement := target - num             // Calculate the complement of the current number.
+		if idx, ok := numMap[complement]; ok { // Check if the complement is already in the map.
+			return []int{idx, i} // If found, return the indices of the complement and the current number.
+		}
+		numMap[num] = i // Add the current number and its index to the map.
+	}
+	return []int{} // If no solution is found, return an empty slice.
+}
